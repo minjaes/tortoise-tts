4ec1f62cfcbd090d7d09968897c37abd7d82e4fd
MJ <minjaes91@gmail.com>
2024-01-05 13:00:22 +0900

    test

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="tortoise-tts",
+    name="tortoise",
     packages=setuptools.find_packages(),
     version="3.0.0",
     author="James Betker",
